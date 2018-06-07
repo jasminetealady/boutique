0c9d30d915aec628ee72ea8e26a78342565d0155
Razelle McCarrick <razelle@satyaorthopedic.com>
2018-06-07 19:30:15 -0400

    user create action and user_params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,17 @@ class UsersController < ApplicationController
   end
 
   def create
-
+    if params[:first_name].present? && params[:last_name].present? && params[:email].present? && params[:password].present?
+    @user = User.create(user_params)
+    redirect_to login_path
+    else
+      flash[:message] = "Yo you need to fill those fields homie"
+      redirect_to signup_path
+    end
   end
+
+  private
+    def user_params
+      params.require(:user).permit(:first_name, :last_name, :email, :password)
+    end
 end
